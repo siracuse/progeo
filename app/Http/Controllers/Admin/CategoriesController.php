ac949206cd5d35f0e50d7f47db6da97d4d35cafd
siracuse <siracuse.harichandra@gmail.com>
2019-02-03 17:05:45 +0100

    'CRUD-categorie'

---
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoriesController extends Controller
+{
+    public function getAll () {
+        $categories = Category::get(['id', 'name']);
+        return view ('admin.category_list',[ 'categories' => $categories]);
+    }
+
+    public function getNew (Request $request) {
+        if ($request->input('name') !== null) {
+            $this->validate($request, ['name' => 'required']);
+            $category = new Category ();
+            $category->name = $request->input('name');
+            $category->save();
+            return redirect()->route('category_list');
+        }
+        return view('admin.category_new');
+    }
+
+    public function getEdit ($category_id) {
+        $category = Category::findOrFail($category_id);
+        return view ('admin.category_edit',[ 'category' => $category]);
+    }
+
+    public function postEdit (Request $request) {
+        $this->validate($request, ['name' => 'required']);
+        $category = Category::findOrFail($request->input('category_id'));
+        $category->name = $request->input('name');
+        $category->save();
+        return redirect()->route('category_list');
+    }
+
+    public function getDelete ($category_id) {
+        $category = Category::findOrFail($category_id);
+        $category->delete();
+        return redirect()->route('category_list');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,30 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
 
 
-
 //Page d'accueil avec la carte
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+
+
+
+//Liste categorie
+Route::get ('/admin/category/list', 'Admin\CategoriesController@getAll')->name('category_list');
+
+//Ajout categorie
+Route::get ('/admin/category/new', 'Admin\CategoriesController@getNew')->name('category_new');
+Route::post ('/admin/category/new', 'Admin\CategoriesController@getNew')->name('category_new_post');
+
+//Modification categorie
+Route::get ('/admin/category/edit/{category_id}', 'Admin\CategoriesController@getEdit')->name('category_edit');
+Route::post ('/admin/category/edit', 'Admin\CategoriesController@postEdit')->name('category_edit_post');
+
+//Suppression categorie
+Route::get ('/admin/category/delete/{category_id}', 'Admin\CategoriesController@getDelete')->name('category_delete');
+
