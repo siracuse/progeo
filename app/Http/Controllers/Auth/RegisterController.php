e911256db6dff406571980b3ec11f72a2fe2c42d
MIW <samuel.place@etu.univ-amu.fr>
2019-02-07 10:20:31 +0100

    test2

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -48,8 +48,10 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'name' => 'required|string|max:30',
+            'first_name'=> 'required|string|max:30',
+            'email' => 'required|string|email|max:50|unique:users',
+            'phone'=>'required|string|max:10',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
@@ -64,8 +66,11 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'firstname' => $data['first_name'],
             'email' => $data['email'],
-            'password' => bcrypt($data['password']),
+            'phone' => $data['phone'],
+            'password' => bcrypt($data['password'])
+
         ]);
     }
 }
