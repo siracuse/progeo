e9da6c86366686b658cabe888608d3f7f7b53896
MIW <samuel.place@etu.univ-amu.fr>
2019-03-06 10:32:42 +0100

    recup master

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -27,8 +27,31 @@
                 <li><a href="">Nous contacter</a></li>
             </ul>
             <ul>
-                <li><a class="btn-inscription" href="{{ route('register') }}">Inscription</a></li>
-                <li><a class="btn1" href="{{ route('login') }}">Connexion</a></li>
+                <!-- Authentication Links -->
+                @guest
+                    <li><a class="btn-inscription" href="{{ route('register') }}">Inscription</a></li>
+                    <li><a class="btn1" href="{{ route('login') }}">Connexion</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Deconnexion
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
             </ul>
         </nav>
     </header>
