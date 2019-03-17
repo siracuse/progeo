9b593ae397f097e2f937b40f879b477ffb597c1c
siracuse <siracuse.harichandra@gmail.com>
2019-03-17 17:34:03 +0100

    inté admin : home + store

--- resources/views/admin/store_list.blade.php
+++ resources/views/admin/store_list.blade.php
@@ -1,32 +1,49 @@
-@extends('layout.app')
+@extends('layout.admin')
 
 @section('content')
-    <h2> Liste des Magasins </h2>
-    <a href="{{url ('admin\store\new')}}">Ajouter un magasin +</a><br><br>
-    <table>
-        <tbody>
-        <tr>
-            <td style="width:100px;"><b>Magasin</b></td>
-            <td style="width:100px;"><b>Responsable</b></td>
-            <td style="width:200px;"><b>Adresse</b></td>
-            <td style="width:70px;"><b>Téléphone</b></td>
-            <td style="width:50px;"><b>Email</b></td>
-            <td style="width:130px;"><b>Actions</b></td>
-        </tr>
-        @foreach($stores as $store )
-            <tr>
-                <td>{{$store->name}} </td>
-                <td>{{$store->user->name}} </td>
-                <td>{{$store->address}} </td>
-                <td>{{$store->phone}} </td>
-                <td>{{$store->email}} </td>
-                <td>
-                    <a href="{{url ('admin\store\edit', ['store_id' => $store->id])}}">Modifier</a> /
-                    <a href="{{url ('admin\store\delete', ['store_id' => $store->id])}}">Supprimer</a>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table><br><br>
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class=" col-lg-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong class="card-title">Liste des Magasins</strong>
+                            <a href="{{url ('admin\store\new')}}" class="pull-right">Ajouter un magasin +</a>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Magasin</th>
+                                    <th scope="col">Responsable</th>
+                                    <th scope="col">Adresse</th>
+                                    <th scope="col">Téléphone</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+
+                                @foreach($stores as $store )
+                                    <tr>
+                                        <td>{{$store->name}} </td>
+                                        <td>{{$store->user->name}} </td>
+                                        <td>{{$store->address}} </td>
+                                        <td>{{$store->phone}} </td>
+                                        <td>{{$store->email}} </td>
+                                        <td>
+                                            <a href="{{url ('admin\store\edit', ['store_id' => $store->id])}}">Modifier</a> /
+                                            <a href="{{url ('admin\store\delete', ['store_id' => $store->id])}}">Supprimer</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     {{$stores->links()}}
 @endsection
