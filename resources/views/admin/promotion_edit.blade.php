b7604c2c287c14824e8a396efac598cfd490f98d
siracuse <siracuse.harichandra@gmail.com>
2019-03-17 19:18:25 +0100

    inté admin promo

--- resources/views/admin/promotion_edit.blade.php
+++ resources/views/admin/promotion_edit.blade.php
@@ -1,45 +1,134 @@
-@extends ('layout.app')
+@extends ('layout.admin')
 
 @section ('content')
-    <a href="{{url ('admin\promotion\list')}}">Retour</a><br><br>
-    <h2>Modification</h2>
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class=" col-lg-12">
 
-    <form method="post" action="{{ route ('promotion_edit_post') }}">
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Edition </strong> d'une promotion
+                        </div>
+                        <div class="card-body card-block">
+                            <form action="{{ route('promotion_edit_post')}}" method="post" class="form-horizontal">
+                                <input type="hidden" name="promotion_id" value="{{$promotion->id}}">
+                                {{ csrf_field() }}
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Nom</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="name" name="name" placeholder="Nom" class="form-control" value="{{old('name', $promotion->name)}}"></div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="dateStart" class=" form-control-label">Date début</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="dateStart" name="dateStart" placeholder="Adresse" class="form-control" value="{{old('name', $promotion->startDate)}}"></div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="dateEnd" class=" form-control-label">Date fin</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="dateEnd" name="dateEnd" placeholder="Ville" class="form-control" value="{{old('name', $promotion->endDate)}}"></div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="photo1" class=" form-control-label">Photo 1</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="photo1" name="photo1" placeholder="Photo 1" class="form-control" value="{{old('name', $promotion->photo1)}}"></div>
+                                </div>
+                                {{--<div class="row form-group">--}}
+                                {{--<div class="col col-md-3"><label for="file-input" class=" form-control-label">File input</label></div>--}}
+                                {{--<div class="col-12 col-md-9"><input type="file" id="file-input" name="file-input" class="form-control-file"></div>--}}
+                                {{--</div>--}}
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="photo2" class=" form-control-label">Photo 2</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="photo2" name="photo2" placeholder="Photo 2" class="form-control" value="{{old('name', $promotion->photo2)}}"></div>
+                                </div>
+                                {{--<div class="row form-group">--}}
+                                {{--<div class="col col-md-3"><label for="file-input" class=" form-control-label">File input</label></div>--}}
+                                {{--<div class="col-12 col-md-9"><input type="file" id="file-input" name="file-input" class="form-control-file"></div>--}}
+                                {{--</div>--}}
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="storeName" class=" form-control-label">Nom du magasin</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="storeName" name="storeName" placeholder="Nom du magasin" class="form-control" value="{{old('name', $promotion->store->name)}}"></div>
+                                </div>
 
-        <input type="hidden" name="promotion_id" value="{{$promotion->id}}">
-        {{ csrf_field() }}
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label class=" form-control-label">Activer directement la promotion ?</label></div>
+                                    <div class="col col-md-9">
+                                        @if ($promotion->activated === 0)
+                                            <div class="form-check">
+                                                <div class="radio">
+                                                    <label for="oui" class="form-check-label ">
+                                                        <input type="radio" id="oui" name="activated" value="1" class="form-check-input" >Oui
+                                                    </label>
+                                                </div>
+                                                <div class="radio">
+                                                    <label for="non" class="form-check-label ">
+                                                        <input type="radio" id="non" name="activated" value="0" class="form-check-input" checked>Non
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        @else
+                                            <div class="form-check">
+                                                <div class="radio">
+                                                    <label for="oui" class="form-check-label ">
+                                                        <input type="radio" id="oui" name="activated" value="1" class="form-check-input" checked>Oui
+                                                    </label>
+                                                </div>
+                                                <div class="radio">
+                                                    <label for="non" class="form-check-label ">
+                                                        <input type="radio" id="non" name="activated" value="0" class="form-check-input">Non
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
 
-        <label for="name">Nom :</label>
-        <input type="text" class="form-control" value="{{old('name', $promotion->name)}}" id="name" name="name">
+                                <input type="submit" value="Modifier" name="submit" class="btn btn-primary btn-sm">
+                                <input type="reset" value="Annuler"  class="btn btn-danger btn-sm">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <label for="dateStart">Date début :</label>
-        <input type="text" class="form-control" value="{{old('startDate', $promotion->startDate)}}" id="dateStart" name="dateStart">
+    {{--<a href="{{url ('admin\promotion\list')}}">Retour</a><br><br>--}}
+    {{--<h2>Modification</h2>--}}
 
-        <label for="dateEnd">Date fin :</label>
-        <input type="text" class="form-control" value="{{old('endDate', $promotion->endDate)}}" id="dateEnd" name="dateEnd">
+    {{--<form method="post" action="{{ route ('promotion_edit_post') }}">--}}
 
-        <label for="photo1">Photo 1 :</label>
-        <input type="text" class="form-control" value="{{old('photo1', $promotion->photo1)}}" id="photo1" name="photo1">
+        {{--<input type="hidden" name="promotion_id" value="{{$promotion->id}}">--}}
+        {{--{{ csrf_field() }}--}}
 
-        <label for="photo2">Photo 2 :</label>
-        <input type="text" class="form-control" value="{{old('photo2', $promotion->photo2)}}" id="photo2" name="photo2">
+        {{--<label for="name">Nom :</label>--}}
+        {{--<input type="text" class="form-control" value="{{old('name', $promotion->name)}}" id="name" name="name">--}}
 
-        <label for="storeName">Nom du magasin :</label>
-        <input type="text" class="form-control" id="storeName" name="storeName" value="{{old('storeName', $promotion->store->name)}}">
+        {{--<label for="dateStart">Date début :</label>--}}
+        {{--<input type="text" class="form-control" value="{{old('startDate', $promotion->startDate)}}" id="dateStart" name="dateStart">--}}
 
-        <p>Publication :</p>
-        @if ($promotion->activated === 0)
-            <input type="radio" id="oui" name="activated" value="1">
-            <label for="oui">Oui</label>
-            <input type="radio" id="non" name="activated" value="0" checked>
-            <label for="non">Non</label>
-        @else
-            <input type="radio" id="oui" name="activated" value="1" checked>
-            <label for="oui">Oui</label>
-            <input type="radio" id="non" name="activated" value="0">
-            <label for="non">Non</label>
-        @endif
+        {{--<label for="dateEnd">Date fin :</label>--}}
+        {{--<input type="text" class="form-control" value="{{old('endDate', $promotion->endDate)}}" id="dateEnd" name="dateEnd">--}}
 
-        <br><br><input type="submit" value="Ajouter"><br><br><br><br><br><br>
-    </form>
+        {{--<label for="photo1">Photo 1 :</label>--}}
+        {{--<input type="text" class="form-control" value="{{old('photo1', $promotion->photo1)}}" id="photo1" name="photo1">--}}
+
+        {{--<label for="photo2">Photo 2 :</label>--}}
+        {{--<input type="text" class="form-control" value="{{old('photo2', $promotion->photo2)}}" id="photo2" name="photo2">--}}
+
+        {{--<label for="storeName">Nom du magasin :</label>--}}
+        {{--<input type="text" class="form-control" id="storeName" name="storeName" value="{{old('storeName', $promotion->store->name)}}">--}}
+
+        {{--<p>Publication :</p>--}}
+        {{--@if ($promotion->activated === 0)--}}
+            {{--<input type="radio" id="oui" name="activated" value="1">--}}
+            {{--<label for="oui">Oui</label>--}}
+            {{--<input type="radio" id="non" name="activated" value="0" checked>--}}
+            {{--<label for="non">Non</label>--}}
+        {{--@else--}}
+            {{--<input type="radio" id="oui" name="activated" value="1" checked>--}}
+            {{--<label for="oui">Oui</label>--}}
+            {{--<input type="radio" id="non" name="activated" value="0">--}}
+            {{--<label for="non">Non</label>--}}
+        {{--@endif--}}
+
+        {{--<br><br><input type="submit" value="Ajouter"><br><br><br><br><br><br>--}}
+    {{--</form>--}}
 @endsection
--- resources/views/admin/promotion_new.blade.php
+++ resources/views/admin/promotion_new.blade.php
@@ -1,36 +1,77 @@
-@extends ('layout.app')
+@extends ('layout.admin')
 
 @section ('content')
+    <div class="content mt-3">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class=" col-lg-12">
 
-    <a href="{{url ('admin\promotion\list')}}">Retour</a><br><br>
-    <h2>Ajout d'une promotion</h2>
+                    <div class="card">
+                        <div class="card-header">
+                            <strong>Ajout </strong> d'un magasin
+                        </div>
+                        <div class="card-body card-block">
+                            <form action="{{ route('promotion_new')}}" method="post" class="form-horizontal">
+                                {{ csrf_field() }}
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="name" class=" form-control-label">Nom</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="name" name="name" placeholder="Nom" class="form-control" value="azed"></div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="dateStart" class=" form-control-label">Date début</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="dateStart" name="dateStart" placeholder="Adresse" class="form-control" value="2005-02-07 07:46:21"></div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="dateEnd" class=" form-control-label">Date fin</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="dateEnd" name="dateEnd" placeholder="Ville" class="form-control" value="2005-02-07 07:46:21"></div>
+                                </div>
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="photo1" class=" form-control-label">Photo 1</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="photo1" name="photo1" placeholder="Photo 1" class="form-control" value="azed"></div>
+                                </div>
+                                {{--<div class="row form-group">--}}
+                                {{--<div class="col col-md-3"><label for="file-input" class=" form-control-label">File input</label></div>--}}
+                                {{--<div class="col-12 col-md-9"><input type="file" id="file-input" name="file-input" class="form-control-file"></div>--}}
+                                {{--</div>--}}
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="photo2" class=" form-control-label">Photo 2</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="photo2" name="photo2" placeholder="Photo 2" class="form-control" value="azed"></div>
+                                </div>
+                                {{--<div class="row form-group">--}}
+                                {{--<div class="col col-md-3"><label for="file-input" class=" form-control-label">File input</label></div>--}}
+                                {{--<div class="col-12 col-md-9"><input type="file" id="file-input" name="file-input" class="form-control-file"></div>--}}
+                                {{--</div>--}}
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="storeName" class=" form-control-label">Nom du magasin</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="storeName" name="storeName" placeholder="Nom du magasin" class="form-control" value="Barton Hoppe"></div>
+                                </div>
 
-    <form method="post" action="{{ route ('promotion_new') }}">
-        {{ csrf_field() }}
-        <label for="name">Nom :</label>
-        <input type="text" class="form-control" id="name" name="name" value="bob">
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label class=" form-control-label">Activer la promotion ?</label></div>
+                                    <div class="col col-md-9">
+                                        <div class="form-check">
+                                            <div class="radio">
+                                                <label for="oui" class="form-check-label ">
+                                                    <input type="radio" id="oui" name="activated" value="1" class="form-check-input" checked>Oui
+                                                </label>
+                                            </div>
+                                            <div class="radio">
+                                                <label for="non" class="form-check-label ">
+                                                    <input type="radio" id="non" name="activated" value="0" class="form-check-input">Non
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-        <label for="dateStart">Date début :</label>
-        <input type="text" class="form-control" id="dateStart" name="dateStart" value="2005-02-07 07:46:21">
+                                <input type="submit" value="Ajouter" name="submit" class="btn btn-primary btn-sm">
+                                <input type="reset" value="Annuler"  class="btn btn-danger btn-sm">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <label for="dateEnd">Date fin :</label>
-        <input type="text" class="form-control" id="dateEnd" name="dateEnd" value="2005-02-07 07:46:21">
-
-        <label for="photo1">Photo 1 :</label>
-        <input type="text" class="form-control" id="photo1" name="photo1" value="bob">
-
-        <label for="photo2">Photo 2 :</label>
-        <input type="text" class="form-control" id="photo2" name="photo2" value="bob">
-
-        <label for="storeName">Nom du magasin :</label>
-        <input type="text" class="form-control" id="storeName" name="storeName" value="Hotel F1">
-
-        <p>Activer directement la promotion ? :</p>
-        <input type="radio" id="oui" name="activated" value="1" checked>
-        <label for="oui">Oui</label>
-        <input type="radio" id="non" name="activated" value="0" >
-        <label for="non">Non</label>
-
-        <br><br><input type="submit" value="Ajouter"><br><br><br><br><br><br>
-    </form>
 @endsection
