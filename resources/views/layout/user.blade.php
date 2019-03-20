1f5c536371888ece55c2bf1bdc8f7d39e6477731
MIW <samuel.place@etu.univ-amu.fr>
2019-03-20 11:24:48 +0100

    ajout meta viewport

--- resources/views/layout/user.blade.php
+++ resources/views/layout/user.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
     <title>bob</title>
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
           integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
